de96cda631bcf5f83b6dc3df4ce5f078ca05e69f
Alex Talha <alex.talha@hotmail.com>
2017-01-18 18:55:19 +0000

    18-01-2017 iniciado as buscar por grupo id em updates

--- bot.js
+++ bot.js
@@ -3,14 +3,13 @@
 module.exports = function(){
 
 
-
 var TelegramBot = require('node-telegram-bot-api');
 var fs = require('fs');
 
 
 // replace the value below with the Telegram token you receive from @BotFather
 var token = "318036094:AAEGiNMFRtfK3q1GjNx9fagnWF9pK57wv_M";
-
+             
 // Create a bot that uses 'polling' to fetch new updates
 var bot = new TelegramBot(token, { polling: true });
 
@@ -30,15 +29,20 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
 });
 
 
-bot.onText(/\/help/, function(msg, match) {
+bot.onText(/\/ajuda/, function(msg, match) {
   var fromId = msg.from.id;
-  bot.sendMessage(fromId, "Veremos que ajuda você precisa.\n/.");
+
+  var mensagem = " Em que eu posso te ajudar? \n\n  "+
+  "/pic - retorna sua foto do telegram \n" +
+  "/padaria - diz quem tem que ir na padaria \n "+
+  "/agua [manha/tarde] - diz quem tem que ir pegar água \n"+
+  "/print [url sem http ] - retorna a imagem printada de um site \n";
+  
+  bot.sendMessage(fromId, mensagem);
 });
 
-
-
-// Matches "/echo [whatever]"
-bot.onText(/\/padaria (.+)/, function (msg,match) {
+/*PADARIA*/
+bot.onText(/\/padaria/, function (msg,match) {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -53,13 +57,26 @@ let team = ['Wagner','BTT','Cansado','Thiaguinho','Allan','Pequeno','Tio Chico',
 });
 
 
+
+bot.onText(/\/updates/, function (msg,match) {
+  
+  var xpto= '';
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId, '..updates');
+  bot.getUpdates().then(console.log);
+  
+  
+});
+
+
+
 // Matches "/echo [whatever]"
 bot.onText(/\/agua (.+)/, function (msg,match) {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-let team = ['Wagner','Cansado','Thiaguinho','Allan','Iza','Glorinha','Pequeno','Tio Chico','Indiano','PC','BTT','Igor'];
+let team = ['Wagner','Cansado','Thiaguinho','Allan','Iza','Glorinha','Pequeno','Tio Chico','Indiano','PC','BTT','Igor','Eloildo'];
 
 var chatId = msg.chat.id;
 
@@ -82,30 +99,16 @@ if(match[1] == 'manha'){
   resp = 'Geral Preguiçoso';
 }
 
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-/*PRINTAR UMA PAGINA E DEVOLVER NO BOT */
-
-bot.onText(/\/profilephoto (.+)/, function (msg,match) {
-  
-   var chatId = msg.chat.id;
-   
-  bot.getUserProfilePhotos(chatId);
-
-  
-});
-
-
-bot.onText(/\/testuserphoto$/, function onMessage(msg) {
+   bot.onText(/\/pic/, function onMessage(msg) {
     var chatId = msg.chat.id;
     var userId = msg.from.id;
-
+    
     bot.getUserProfilePhotos(userId, 0, 1).then(function(data){
       bot.sendPhoto(chatId,data.photos[0][0].file_id,{caption: "It's your photo!"});
     });
-
 });
 
 bot.onText(/\/print (.+)/, function (msg,match) {
@@ -114,45 +117,8 @@ bot.onText(/\/print (.+)/, function (msg,match) {
     var resp = 'Esse ai foi o site printado';
     var chatId = msg.chat.id;
     var  printscreen = require('./printscreen')(bot,chatId,match[1]);
-    
-    
-    
-  /*funciona */  
- // bot.sendPhoto(msg.chat.id, '1484321789782.png', {caption: "Printando o Site Selecionado"});
-  
-  //bot.sendPhoto(chatId, fs.createReadStream('./uol.png'), 'Chronoscape by Alexiuss').then(console.log)
-  
-  /**
-  console.log( __dirname+'/uol') ;
-  */
-  
-  /*
-  bot.sendPhoto({
-  chat_id: chatId,
-  caption: 'Telegram Logo',
-  files: {
-    photo: {
-      filename: __dirname + '/uol.png',
-      stream: fs.createReadStream(__dirname +'/uol.png')
-    }
-  }
-}, function (err, msg) {
-  console.log(err);
-  console.log(msg);
+
 });
-
-  */
-  
-  
-     //bot.sendMessage(chatId, resp);
-      
- 
-
-    ///console.log(printscreen);
-    
-   // bot.sendPhoto(chatId, fs.createReadStream(printscreen.file), 'Site').then(console.log)
-});
-
 
 // Listen for any kind of message. There are different kinds of
 // messages.
